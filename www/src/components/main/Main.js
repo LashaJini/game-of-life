7eb5cfa7e014490ae2e2c16fc6f2499335005a96
109149 <109149qwe@gmail.com>
2021-05-07 12:17:40 +0400

    refactor: leave effect

--- www/src/components/main/Main.js
+++ www/src/components/main/Main.js
@@ -8,309 +8,274 @@ const DEAD_COLOR = "#fff";
 const Main = () => {
   const canvasRef = React.useRef();
   const ctxRef = React.useRef();
-  const [cellSize, setCellSize] = React.useState(8);
+  // const [cellSize, setCellSize] = React.useState(8);
   const [strokeColor, setStrokeColor] = React.useState("#eee");
   const [size, setSize] = React.useState({ width: 0, height: 0 });
   const [cellColor, setCellColor] = React.useState("#000");
+  const [rowInput, setRowInput] = React.useState(null);
+  const [colInput, setColInput] = React.useState(null);
+  const cellSize = React.useRef(8);
+  const tickSpeed = React.useRef(1);
+  const animationSpeed = React.useRef(1);
+  const frame = React.useRef(1000);
+  const [playing, setPlaying] = React.useState(false);
+  const [universeState, setUniverseState] = React.useState({
+    rowInput,
+    colInput,
+  });
+  const universe = React.useRef();
+  const animationId = React.useRef();
+  const lastTime = React.useRef(0);
+
+  React.useEffect(() => {
+    universe.current = Universe.new(
+      isPositiveNumber(rowInput) ? rowInput : 32,
+      isPositiveNumber(colInput) ? colInput : 32
+    );
+  }, []);
 
   React.useEffect(() => {
     ctxRef.current = canvasRef.current.getContext("2d");
-    let canvas = canvasRef.current;
-    let ctx = ctxRef.current;
+    let width = universe.current.get_width();
+    let height = universe.current.get_height();
 
-    const animationSpeed = document.querySelector("#animation-speed");
-    const borderColor = document.querySelector("#border-color");
-    const initial = document.querySelector("#initial");
-    const nextGeneration = document.querySelector("#next-generation");
-    const newSize = document.querySelector("#universe-size");
-    const rowInput = document.querySelector("#row-size");
-    const colInput = document.querySelector("#col-size");
-    const reset = document.querySelector("#reset");
-    const cellRange = document.querySelector("#cell-size");
-    const playPause = document.querySelector("#play-pause");
-    const clear = document.querySelector("#clear");
-    const tickSpeed = document.querySelector("#tick-speed");
-
-    let CELL_SIZE = parseInt(cellRange.value);
-    let ALIVE_COLOR = cellColor;
-    let STROKE_COLOR = borderColor.value ? borderColor.value : "#eee";
-    let TICK = parseInt(tickSpeed.value);
-    let ANIMATION_SPEED = parseInt(animationSpeed.value);
-    let animationId = null;
-    let lastTime = 0;
-    let FRAME = 1000 / ANIMATION_SPEED;
-    let rows = Math.floor(parseInt(rowInput.value));
-    let cols = Math.floor(parseInt(colInput.value));
-
-    const universe = Universe.new(
-      isPositiveNumber(rows) ? rows : 32,
-      isPositiveNumber(cols) ? cols : 32
-    );
-    let width = universe.get_width();
-    let height = universe.get_height();
-
-    canvas.width = (CELL_SIZE + 1) * width + 1;
-    canvas.height = (CELL_SIZE + 1) * height + 1;
-
-    const render = (time) => {
-      if (time - lastTime > FRAME) {
-        for (let i = 0; i < TICK; i++) {
-          universe.tick();
-        }
-        drawCells({
-          universe,
-          ctx,
-          cellSize: CELL_SIZE,
-          deadColor: DEAD_COLOR,
-          aliveColor: ALIVE_COLOR,
-          width,
-          height,
-        });
-        lastTime = time;
-      }
-      animationId = requestAnimationFrame(render);
-    };
-
-    const draw = () => {
-      drawGrid({
-        ctx,
-        strokeColor: STROKE_COLOR,
-        cellSize: CELL_SIZE,
-        width,
-        height,
-      });
-      drawCells({
-        universe,
-        ctx,
-        cellSize: CELL_SIZE,
-        deadColor: DEAD_COLOR,
-        aliveColor: ALIVE_COLOR,
-        width,
-        height,
-      });
-    };
-
-    const play = () => {
-      playPause.textContent = "pause";
-
-      animationId = requestAnimationFrame(render);
-      return true;
-    };
-
-    const pause = () => {
-      playPause.textContent = "play";
-
-      cancelAnimationFrame(animationId);
-      animationId = null;
-      return true;
-    };
-
-    animationSpeed.addEventListener("change", (event) => {
-      ANIMATION_SPEED = parseInt(event.target.value);
-      FRAME = 1000 / ANIMATION_SPEED;
-    });
-
-    cellRange.addEventListener("change", (event) => {
-      CELL_SIZE = parseInt(event.target.value);
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      canvas.width = (CELL_SIZE + 1) * width + 1;
-      canvas.height = (CELL_SIZE + 1) * height + 1;
-
-      draw();
-    });
-
-    newSize.addEventListener("click", (event) => {
-      rows = Math.floor(parseInt(rowInput.value));
-      cols = Math.floor(parseInt(colInput.value));
-      if (isPositiveNumber(rows) && isPositiveNumber(cols)) {
-        width = rows;
-        height = cols;
-
-        universe.set_width(width);
-        universe.set_height(height);
-        // we are "emptying" after, because universe must be filled with dead cells.
-        universe.empty();
-
-        canvas.width = (CELL_SIZE + 1) * width + 1;
-        canvas.height = (CELL_SIZE + 1) * height + 1;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        draw();
-      }
-    });
-
-    playPause.addEventListener("click", (event) => {
-      animationId ? pause() : play();
-    });
-
-    tickSpeed.addEventListener("change", (event) => {
-      TICK = parseInt(event.target.value);
-    });
-
-    // cellColor.addEventListener("change", (event) => {
-    //   ALIVE_COLOR = cellColor.value ? cellColor.value : "#000";
-    //   drawCells({
-    //     universe,
-    //     ctx,
-    //     cellSize: CELL_SIZE,
-    //     deadColor: DEAD_COLOR,
-    //     aliveColor: ALIVE_COLOR,
-    //     width,
-    //     height,
-    //   });
-    // });
-
-    borderColor.addEventListener("change", (event) => {
-      STROKE_COLOR = borderColor.value ? borderColor.value : "#eee";
-      drawGrid({
-        ctx,
-        strokeColor: STROKE_COLOR,
-        cellSize: CELL_SIZE,
-        width,
-        height,
-      });
-    });
-
-    canvas.addEventListener("click", (event) => {
-      const boundingRect = canvas.getBoundingClientRect();
-
-      const canvasLeft = event.clientX - boundingRect.left;
-      const canvasTop = event.clientY - boundingRect.top;
-
-      const row = Math.floor(canvasTop / (CELL_SIZE + 1));
-      const col = Math.floor(canvasLeft / (CELL_SIZE + 1));
-
-      universe.toggle_cell(row, col);
-      drawCells({
-        universe,
-        ctx,
-        cellSize: CELL_SIZE,
-        deadColor: DEAD_COLOR,
-        aliveColor: ALIVE_COLOR,
-        width,
-        height,
-      });
-    });
-
-    nextGeneration.addEventListener("click", (event) => {
-      pause();
-      for (let i = 0; i < TICK; i++) {
-        universe.tick();
-      }
-      drawCells({
-        universe,
-        ctx,
-        cellSize: CELL_SIZE,
-        deadColor: DEAD_COLOR,
-        aliveColor: ALIVE_COLOR,
-        width,
-        height,
-      });
-    });
-
-    initial.addEventListener("click", (event) => {
-      pause();
-      universe.random();
-      drawCells({
-        universe,
-        ctx,
-        cellSize: CELL_SIZE,
-        deadColor: DEAD_COLOR,
-        aliveColor: ALIVE_COLOR,
-        width,
-        height,
-      });
-    });
-
-    clear.addEventListener("click", (event) => {
-      pause();
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      universe.empty();
-      draw();
-    });
-
-    reset.addEventListener("click", (event) => {
-      pause();
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      universe.reset();
-      draw();
-    });
-
+    canvasRef.current.width = (cellSize.current + 1) * width + 1;
+    canvasRef.current.height = (cellSize.current + 1) * height + 1;
     draw();
-  }, [cellColor]);
+  }, []);
 
-  const handlePlay = () => {
-    // setPlay((prev) => !prev);
+  const handlePlay = (event, pause) => {
+    setPlaying(() => {
+      if (!animationId.current && !pause) {
+        animationId.current = requestAnimationFrame(render);
+        return true;
+      }
+
+      cancelAnimationFrame(animationId.current);
+      animationId.current = null;
+      return false;
+    });
   };
 
-  const drawGrid = ({ ctx, strokeColor, cellSize, width, height }) => {
-    ctx.beginPath();
-    ctx.strokeStyle = strokeColor;
+  const drawGrid = ({ width, height }) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.strokeStyle = strokeColor;
 
     for (let i = 0; i <= width; i++) {
-      ctx.moveTo(i * (cellSize + 1) + 1, 0);
-      ctx.lineTo(i * (cellSize + 1) + 1, (cellSize + 1) * height + 1);
+      ctxRef.current.moveTo(i * (cellSize.current + 1) + 1, 0);
+      ctxRef.current.lineTo(
+        i * (cellSize.current + 1) + 1,
+        (cellSize.current + 1) * height + 1
+      );
     }
 
     for (let i = 0; i <= height; i++) {
-      ctx.moveTo(0, i * (cellSize + 1) + 1);
-      ctx.lineTo((cellSize + 1) * width + 1, i * (cellSize + 1) + 1);
+      ctxRef.current.moveTo(0, i * (cellSize.current + 1) + 1);
+      ctxRef.current.lineTo(
+        (cellSize.current + 1) * width + 1,
+        i * (cellSize.current + 1) + 1
+      );
     }
 
-    ctx.stroke();
+    ctxRef.current.stroke();
   };
 
-  const drawCells = ({
-    universe,
-    ctx,
-    cellSize,
-    deadColor,
-    aliveColor,
-    width,
-    height,
-  }) => {
-    const cellsPtr = universe.get_cells();
+  const drawCells = ({ width, height }) => {
+    const cellsPtr = universe.current.get_cells();
     const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
-    ctx.beginPath();
+    ctxRef.current.beginPath();
 
     // Alive cells.
-    ctx.fillStyle = aliveColor;
+    ctxRef.current.fillStyle = cellColor;
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
         const idx = getIndex(width, row, col);
         if (cells[idx] !== Cell.Alive) continue;
 
-        ctx.fillRect(
-          col * (cellSize + 1) + 2,
-          row * (cellSize + 1) + 2,
-          cellSize - 1,
-          cellSize - 1
+        ctxRef.current.fillRect(
+          col * (cellSize.current + 1) + 2,
+          row * (cellSize.current + 1) + 2,
+          cellSize.current - 1,
+          cellSize.current - 1
         );
       }
     }
 
     // Dead cells.
-    ctx.fillStyle = deadColor;
+    ctxRef.current.fillStyle = DEAD_COLOR;
     for (let row = 0; row < height; row++) {
       for (let col = 0; col < width; col++) {
         const idx = getIndex(width, row, col);
         if (cells[idx] !== Cell.Dead) continue;
 
-        ctx.fillRect(
-          col * (cellSize + 1) + 2,
-          row * (cellSize + 1) + 2,
-          cellSize - 1,
-          cellSize - 1
+        ctxRef.current.fillRect(
+          col * (cellSize.current + 1) + 2,
+          row * (cellSize.current + 1) + 2,
+          cellSize.current - 1,
+          cellSize.current - 1
         );
       }
     }
 
-    ctx.fill();
+    ctxRef.current.fill();
   };
 
   const cellColorChange = (event) => {
     setCellColor(event.target.value || "#000");
+  };
+
+  const strokeColorChange = (event) => {
+    setStrokeColor(event.target.value || "#eee");
+  };
+
+  const handleRowChange = (event) => {
+    setRowInput(Math.floor(parseInt(event.target.value)));
+  };
+  const handleColChange = (event) => {
+    setColInput(Math.floor(parseInt(event.target.value)));
+  };
+
+  const changeUniverseSize = () => {
+    setUniverseState((prev) => {
+      return {
+        ...prev,
+        rowInput,
+        colInput,
+      };
+    });
+
+    if (isPositiveNumber(rowInput) && isPositiveNumber(colInput)) {
+      let width = rowInput;
+      let height = colInput;
+
+      universe.current.set_width(width);
+      universe.current.set_height(height);
+      // we are "emptying" after, because universe must be filled with dead cells.
+      universe.current.empty();
+
+      canvasRef.current.width = (cellSize.current + 1) * width + 1;
+      canvasRef.current.height = (cellSize.current + 1) * height + 1;
+      ctxRef.current.clearRect(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      draw(width, height);
+    }
+  };
+
+  const draw = (width, height) => {
+    drawGrid({
+      width: width || universe.current.get_width(),
+      height: height || universe.current.get_width(),
+    });
+    drawCells({
+      width: width || universe.current.get_width(),
+      height: height || universe.current.get_width(),
+    });
+  };
+
+  const changeTickSpeed = (event) => {
+    tickSpeed.current = parseInt(event.target.value);
+  };
+
+  const changeCellSize = (event) => {
+    let newCellSize = parseInt(event.target.value);
+    // setCellSize(newCellSize);
+    cellSize.current = newCellSize;
+    ctxRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    canvasRef.current.width =
+      (newCellSize + 1) * universe.current.get_width() + 1;
+    canvasRef.current.height =
+      (newCellSize + 1) * universe.current.get_height() + 1;
+    draw();
+  };
+
+  const changeAnimationSpeed = (event) => {
+    let tmp = parseInt(event.target.value);
+    animationSpeed.current = tmp;
+    frame.current = 1000 / tmp;
+  };
+
+  const render = (time) => {
+    if (time - lastTime.current > frame.current) {
+      for (let i = 0; i < tickSpeed.current; i++) {
+        universe.current.tick();
+      }
+      drawCells({
+        width: universe.current.get_width(),
+        height: universe.current.get_height(),
+      });
+      lastTime.current = time;
+    }
+    animationId.current = requestAnimationFrame(render);
+  };
+
+  const nextGeneration = (event) => {
+    handlePlay(null, true);
+    for (let i = 0; i < tickSpeed.current; i++) {
+      universe.current.tick();
+    }
+    drawCells({
+      width: universe.current.get_width(),
+      height: universe.current.get_height(),
+    });
+  };
+
+  const initial = () => {
+    handlePlay(null, true);
+    universe.current.random();
+    drawCells({
+      width: universe.current.get_width(),
+      height: universe.current.get_height(),
+    });
+  };
+
+  const clear = () => {
+    handlePlay(null, true);
+    ctxRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    universe.current.empty();
+    draw();
+  };
+
+  const reset = () => {
+    handlePlay(null, true);
+    ctxRef.current.clearRect(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+    universe.current.reset();
+    draw();
+  };
+
+  const toggleCell = (event) => {
+    const boundingRect = canvasRef.current.getBoundingClientRect();
+
+    const canvasLeft = event.clientX - boundingRect.left;
+    const canvasTop = event.clientY - boundingRect.top;
+
+    const row = Math.floor(canvasTop / (cellSize.current + 1));
+    const col = Math.floor(canvasLeft / (cellSize.current + 1));
+
+    universe.current.toggle_cell(row, col);
+    drawCells({
+      width: universe.current.get_width(),
+      height: universe.current.get_height(),
+    });
   };
 
   console.log("render");
@@ -318,28 +283,60 @@ const Main = () => {
     <>
       <div>
         <button id="play-pause" onClick={handlePlay}>
-          play
+          {playing ? "pause" : "play"}
         </button>
-        <button id="next-generation">next generation</button>
+        <button id="next-generation" onClick={nextGeneration}>
+          next generation
+        </button>
       </div>
       <div>
-        <button id="initial">initial</button>
-        <button id="clear">clear</button>
-        <button id="reset">reset</button>
+        <button id="initial" onClick={initial}>
+          initial
+        </button>
+        <button id="clear" onClick={clear}>
+          clear
+        </button>
+        <button id="reset" onClick={reset}>
+          reset
+        </button>
       </div>
       <div>
         <label htmlFor="cell-size">cell size</label>
-        4<input id="cell-size" type="range" min="4" max="20" />
+        4
+        <input
+          id="cell-size"
+          type="range"
+          min="4"
+          max="20"
+          onChange={changeCellSize}
+          defaultValue={cellSize.current}
+        />
         20
       </div>
       <div>
         <label htmlFor="tick-speed">generation skip</label>
-        1<input id="tick-speed" type="range" min="1" max="10" />
+        1
+        <input
+          id="tick-speed"
+          type="range"
+          min="1"
+          max="10"
+          onChange={changeTickSpeed}
+          defaultValue={tickSpeed.current}
+        />
         10
       </div>
       <div>
         <label htmlFor="animation-speed">animation speed</label>
-        1<input id="animation-speed" type="range" min="1" max="30" />
+        1
+        <input
+          id="animation-speed"
+          type="range"
+          min="1"
+          max="30"
+          onChange={changeAnimationSpeed}
+          defaultValue={animationSpeed.current}
+        />
         30
       </div>
       <div>
@@ -351,15 +348,22 @@ const Main = () => {
         />
         <label htmlFor="cell-color">cell color</label>
         <br />
-        <input type="color" id="border-color" />
+        <input
+          type="color"
+          id="border-color"
+          onChange={strokeColorChange}
+          value={strokeColor}
+        />
         <label htmlFor="border-color">border color</label>
       </div>
       <div>
-        <button id="universe-size">new size</button>
-        <input type="text" size="10" id="row-size" />
-        <input type="text" size="10" id="col-size" />
+        <button id="universe-size" onClick={changeUniverseSize}>
+          new size
+        </button>
+        <input type="text" size="10" id="row-size" onChange={handleRowChange} />
+        <input type="text" size="10" id="col-size" onChange={handleColChange} />
       </div>
-      <canvas className="scene" ref={canvasRef}></canvas>
+      <canvas className="scene" ref={canvasRef} onClick={toggleCell}></canvas>
     </>
   );
 };
