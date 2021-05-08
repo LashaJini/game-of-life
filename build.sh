d065dd09a3d4aa378bf1085c9bb3fa84ae09a131
109149 <109149qwe@gmail.com>
2021-05-08 14:55:56 +0400

    config: optimize webpack

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd www \
+  && npm run build:prod
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -10,72 +10,79 @@ const ESLintPlugin = require("eslint-webpack-plugin");
 const outDir = "dist";
 const PORT = 3000;
 
-module.exports = {
-  entry: "./src/index",
-  // mode: "development",
-  output: {
-    path: path.join(__dirname, outDir),
-    filename: "[name].bundle.js",
-    // filename: "[name].[contenthash].js",
-  },
-  devServer: {
-    contentBase: path.join(__dirname, outDir),
-    port: PORT,
-    compress: true,
-    hot: true,
-  },
-  devtool: "source-map",
-  resolve: { extensions: ["*", ".js", ".jsx"] },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // "style-loader",
-          "css-loader",
-          // {
-          //   loader: "postcss-loader",
-          //   options: {
-          //     postcssOptions: {
-          //       plugins: [
-          //         [
-          //           "postcss-preset-env",
-          //           {
-          //             // Options
-          //           },
-          //         ],
-          //       ],
-          //     },
-          //   },
-          // },
-        ],
-      },
+module.exports = function (webpackEnv) {
+  const isProd = webpackEnv === "production";
+  const isDev = webpackEnv === "development";
+
+  return {
+    entry: "./src/index",
+    output: {
+      path: path.join(__dirname, outDir),
+      filename: isProd ? "[name].[contenthash:8].js" : "[name].bundle.js",
+      chunkFilename: isProd
+        ? "[name].[contenthash:8].chunk.js"
+        : "[name].chunk.js",
+      // filename: "[name].[contenthash].js",
+    },
+    devServer: {
+      contentBase: path.join(__dirname, outDir),
+      port: PORT,
+      compress: true,
+      hot: true,
+    },
+    devtool: isDev ? "source-map" : false,
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: "babel-loader",
+          options: { presets: ["@babel/env"] },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            // "style-loader",
+            "css-loader",
+            // {
+            //   loader: "postcss-loader",
+            //   options: {
+            //     postcssOptions: {
+            //       plugins: [
+            //         [
+            //           "postcss-preset-env",
+            //           {
+            //             // Options
+            //           },
+            //         ],
+            //       ],
+            //     },
+            //   },
+            // },
+          ],
+        },
+      ],
+    },
+    experiments: {
+      asyncWebAssembly: true,
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        inject: true,
+        minifyJS: true,
+        minifyCSS: true,
+      }),
+      new MiniCssExtractPlugin({
+        // filename: "static/css/[name].[contenthash:8].css",
+        // chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new ReactRefreshWebpackPlugin(),
+      new ESLintPlugin(),
     ],
-  },
-  experiments: {
-    asyncWebAssembly: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      inject: true,
-      minifyJS: true,
-      minifyCSS: true,
-    }),
-    new MiniCssExtractPlugin({
-      // filename: "static/css/[name].[contenthash:8].css",
-      // chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin(),
-    new ESLintPlugin(),
-  ],
+  };
 };
